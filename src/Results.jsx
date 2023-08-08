a7b61e48c0ad79600ee3609f289f3c4cbee2ae9b
tolgayonter <tolgayonter@gmail.com>
2023-08-08 10:16:40 +0300

    add route to pet details

--- src/Results.jsx
+++ src/Results.jsx
@@ -9,6 +9,7 @@ const Results = ({ pets }) => {
         pets.map((pet) => (
           <Pet
             key={pet.id}
+            id={pet.id}
             name={pet.name}
             animal={pet.animal}
             breed={pet.breed}
